c31e74cf861f68ebd9fb1a8acd9b755ef0304bc0
Wai Yan Min Htet <waiyanminhtetmdy786@gmail.com>
2023-12-22 17:42:41 +0630

    resolve the error of cant unfav from the fav page

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const favLayout = document.querySelector('#fav-layout');
 const favBtn = document.querySelector('#favBtn');
 const allBtn = document.querySelector('#all');
 const noFavBookText = document.querySelector('#no-fav-book')
+let curMode = 'all';
 
 // create new book
 function createBook (title,imagePath,filePath) {
@@ -32,14 +33,6 @@ function displayAllBooks (title,imagePath,filePath,fav) {
     </div>
     `;
 }
-
-// add custom method to Storage obj
-// Storage.prototype.setObj = function(key, obj) {
-//     return this.setItem(key, JSON.stringify(obj))
-// }
-// Storage.prototype.getObj = function(key) {
-//     return JSON.parse(this.getItem(key))
-// }
 
 // sample
 let adventureOfSherlockHolmes = new createBook('The Adventure of Sherlock Holmes','./asset/book-images/adventure-of-sherlock-holmes.jpg','./book-dir/adventure-of-sherlock-holmes.html');
@@ -90,55 +83,10 @@ allBooks.forEach(book => {
 });
 mainLayout.innerHTML = allBooksHtml;
 
-// add to fav btn click
-let cardBody = document.querySelectorAll('.card-body');
-cardBody.forEach(card => {
-    card.addEventListener('click', e => {
-        let current = e.target;
-        if(current.matches('button')) {
-            // add to the fav
-           if(current.dataset.action === "add") {
-                let currentFav = JSON.parse(localStorage.getItem('data')) || favBooks;
-
-                // change UI button
-                current.classList.add('d-none');
-                current.parentNode.lastElementChild.classList.remove('d-none');
-
-                // add to fav array
-                let name = current.parentNode.firstElementChild.textContent;
-                
-                allBooks.forEach(book => {
-                    if (book.title === name) {
-                        currentFav.push(book)
-                    }
-                });
-                localStorage.setItem('data',JSON.stringify(currentFav));
-           }
-
-           // remove from the fav
-           if(current.dataset.action==="remove") {
-                // change UI button
-                current.classList.add('d-none');
-                current.previousElementSibling.classList.remove('d-none');
-
-                // remove from fav array
-                let name = current.parentNode.firstElementChild.textContent;
-                
-                favBooks = JSON.parse(localStorage.getItem('data'));
-                favBooks.forEach(book => {
-                    if (book.title == name) {
-                        favBooks.splice(favBooks.findIndex(book => book.title == name),1);
-                    }
-                });
-                localStorage.setItem('data',JSON.stringify(favBooks));
-           }
-        }
-    })
-});
-
 // fav btn click 
 favBtn.addEventListener('click', e => {
     let favBooksHtml = '';
+    curMode = 'fav';
     // loop allBooks and DISPLAY each book in UI
     let currentFav = JSON.parse(localStorage.getItem('data')) || favBooks;
 
@@ -167,11 +115,61 @@ favBtn.addEventListener('click', e => {
     }
 })
 
-
 // all btn click
 allBtn.addEventListener('click', e => {
+    curMode = 'all';
     window.location.reload();
 });
+
+// add to fav btn click
+mainLayout.addEventListener('click', e => {
+    let current = e.target;
+    if(current.matches('button')) {
+        // add to the fav
+       if(current.dataset.action === "add") {
+            let currentFav = JSON.parse(localStorage.getItem('data')) || favBooks;
+
+            // change UI button
+            current.classList.add('d-none');
+            current.parentNode.lastElementChild.classList.remove('d-none');
+
+            // add to fav array
+            let name = current.parentNode.firstElementChild.textContent;
+            
+            allBooks.forEach(book => {
+                if (book.title === name) {
+                    currentFav.push(book)
+                }
+            });
+            localStorage.setItem('data',JSON.stringify(currentFav));
+       }
+
+       // remove from the fav
+       if(current.dataset.action==="remove") {
+        
+            // change UI button
+            current.classList.add('d-none');
+            current.previousElementSibling.classList.remove('d-none');
+
+            // remove card from the ui if from fav page
+            if(curMode === 'fav') {
+                e.target.parentNode.parentNode.remove();
+            }
+
+            // remove from fav array
+            let name = current.parentNode.firstElementChild.textContent;
+            
+            favBooks = JSON.parse(localStorage.getItem('data'));
+            favBooks.forEach(book => {
+                if (book.title == name) {
+                    favBooks.splice(favBooks.findIndex(book => book.title == name),1);
+                }
+            });
+            localStorage.setItem('data',JSON.stringify(favBooks));
+            
+       }
+    }
+})
 
 
 
